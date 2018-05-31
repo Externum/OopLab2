12d092c8bf2aa0dd5a82fb6783633043e1631ee9
roger_brn <roger_brn@bk.ru>
2018-05-31 13:31:01 +0700

    Тесты учителя

--- UnitTest1/ParticipantsTests/GroupTest.cs
+++ UnitTest1/ParticipantsTests/GroupTest.cs
@@ -20,5 +20,42 @@ namespace UnitTest1.ParticipantsTests
             group1.RemoveStudent(student2);
             group1.RemoveStudent(student1);
         }
+
+        [TestMethod]
+        public void GroupShowStudentsTest()
+        {
+            Group group1 = new Group("Группа1");
+            Group group2 = new Group("Группа2");
+            Student student1 = new Student("Студент", "Один");
+            Student student2 = new Student("Студент", "Два");
+            group1.AddStudent(student1);
+            group1.AddStudent(student2);
+            group1.ShowStudents();
+            group2.ShowStudents();
+        }
+
+        [TestMethod]
+        public void GroupAddRemoveSubjectTest()
+        {
+            Group group1 = new Group("Группа1");
+            Subject subject1 = new Subject("Предмет1");
+            Subject subject2 = new Subject("Предмет2");
+            group1.AddSubject(subject1);
+            group1.RemoveSubject(subject2);
+            group1.RemoveSubject(subject1);
+        }
+
+        [TestMethod]
+        public void GroupShowSubjectTest()
+        {
+            Group group1 = new Group("Группа1");
+            Group group2 = new Group("Группа2");
+            Subject subject1 = new Subject("Предмет1");
+            Subject subject2 = new Subject("Предмет2");
+            group1.AddSubject(subject1);
+            group1.AddSubject(subject2);
+            group1.ShowSubjects();
+            group2.ShowSubjects();
+        }
     }
 }
--- UnitTest1/ParticipantsTests/TeacherTest.cs
+++ UnitTest1/ParticipantsTests/TeacherTest.cs
@@ -8,11 +8,30 @@ namespace UnitTest1.ParticipantsTests
     public class TeacherTest
     {
         [TestMethod]
-        public void TeacherAddSubject()
+        public void TeacherAddRemoveGroupTest()
         {
             Teacher teacher1 = new Teacher("Препод", "Один");
-            Subject subject1 = new Subject("Предмет");
-            teacher1.AddSubject(subject1);
+            Teacher teacher2 = new Teacher("Препод", "Два");
+            Group group1 = new Group("Группа1");
+            Group group2 = new Group("Группа2");
+            teacher1.AddStudentGroup(group1);
+            teacher1.AddStudentGroup(group2);
+            teacher2.AddStudentGroup(group1);
+            teacher1.RemoveStudentGroup(group1);
+            teacher2.RemoveStudentGroup(group2);
+        }
+
+        [TestMethod]
+        public void TeacherShowGroupTest()
+        {
+            Teacher teacher1 = new Teacher("Препод", "Один");
+            Teacher teacher2 = new Teacher("Препод", "Два");
+            Group group1 = new Group("Группа1");
+            Group group2 = new Group("Группа2");
+            teacher1.AddStudentGroup(group1);
+            teacher1.AddStudentGroup(group2);
+            teacher1.ShowStudentsGroups();
+            teacher2.ShowStudentsGroups();
         }
     }
 }
