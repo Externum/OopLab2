9164350ab68d0476a79c354ce096ed287f47bc84
roger_brn <roger_brn@bk.ru>
2018-05-31 10:04:34 +0700

    Создал тестовые классы

--- UnitTests/ParticipantsTests/GroupTest.cs
+++ UnitTests/ParticipantsTests/GroupTest.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OopLab2._2;
 
 namespace UnitTests.ParticipantsTests
 {
     class GroupTest
     {
+        public GroupTest()
+        {
+            Group testGroup = new Group("ТестоваяГруппа");
+        }
     }
 }
