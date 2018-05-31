1b428db3e86c3ea1f8e2ab81311d98ebb7facbab
roger_brn <roger_brn@bk.ru>
2018-05-31 13:36:31 +0700

    Тесты для Subject

--- UnitTest1/SubjectsTest/SubjectTest.cs
+++ UnitTest1/SubjectsTest/SubjectTest.cs
@@ -8,11 +8,31 @@ namespace UnitTest1.SubjectsTest
     public class SubjectTest
     {
         [TestMethod]
-        public void SubjectAddTeacherTest()
+        public void SubjectAddRemoveTeacherTest()
         {
-            Subject subject1 = new Subject("Предмет");
-            Teacher teacher = new Teacher("Препод", "Один");
-            subject1.AddTeacher(teacher);
+            Subject subject1 = new Subject("Предмет1");
+            Subject subject2 = new Subject("Предмет2");
+            Teacher teacher1 = new Teacher("Препод", "Один");
+            Teacher teacher2 = new Teacher("Препод", "Два");
+            subject1.AddTeacher(teacher1);
+            subject1.AddTeacher(teacher2);
+            subject1.AddTeacher(teacher1);
+            subject2.AddTeacher(teacher1);
+            subject1.RemoveTeacher(teacher1);
+            subject2.RemoveTeacher(teacher2);
+        }
+
+        [TestMethod]
+        public void SubjectShowTeacherTest()
+        {
+            Subject subject1 = new Subject("Предмет1");
+            Subject subject2 = new Subject("Предмет2");
+            Teacher teacher1 = new Teacher("Препод", "Один");
+            Teacher teacher2 = new Teacher("Препод", "Два");
+            subject1.AddTeacher(teacher1);
+            subject1.AddTeacher(teacher2);
+            subject1.ShowTeachers();
+            subject2.ShowTeachers();
         }
     }
 }
